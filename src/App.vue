076b8667be6cbb3ad5ca9884a111e2d326643c08
MonsPavel <44977810+MonsPavel@users.noreply.github.com>
2022-07-14 21:57:25 +0300

    add tasks logic (#2)

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,31 @@
 <script setup>
+import { computed, ref } from 'vue';
+import ArticleTask from './components/tasks/ArticleTask.vue';
+import ListTask from './components/tasks/ListTask.vue';
+import SearchTask from './components/tasks/SearchTask.vue';
+import WidgetTask from './components/tasks/WidgetTask.vue';
 
+const step = ref(1);
+
+const component = computed(() => {
+  switch (step.value) {
+    case 1:
+      return ArticleTask;
+    case 2:
+      return ListTask;
+    case 3:
+      return SearchTask;
+    case 4:
+      return WidgetTask;
+    default:
+      return ArticleTask;
+  }
+});
 </script>
 
 <template>
   <div>
-
+    <component :is="component"/>
   </div>
 </template>
 
---
+++ src/components/tasks/WidgetTask.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    Виджет
+  </div>
+</template>
+
+<style scoped>
+
+</style>
