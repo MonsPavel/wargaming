0de407ee09ab74452fc56e586e9007ca4a82f03b
Mons Pavel <monspavel@gmail.com>
2022-07-14 21:56:50 +0300

    add tasks logic

---
+++ src/components/tasks/ListTask.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    Адаптивный список
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/tasks/SearchTask.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    Поиск
+  </div>
+</template>
+
+<style scoped>
+
+</style>
