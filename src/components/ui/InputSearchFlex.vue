04d23cec081e30a74abfe9eec208668aa4f4ef12
Mons Pavel <monspavel@gmail.com>
2022-07-21 15:31:33 +0300

    search inputs improvement

--- src/components/ui/InputSearchFlex.vue
+++ src/components/ui/InputSearchFlex.vue
@@ -3,11 +3,37 @@
 </script>
 
 <template>
-  <div class="search-flex">
-    <Icon name="find" class="input-search"/>
-    <input type="text" class="input search-flex-input">
-    <Icon name="cancel" class="input-cancel"/>
+  <div class="search search-flex">
+    <div class="search-flex-wrap">
+      <Icon name="find" class="input-search"/>
+      <input type="text" class="input search-flex-input">
+      <Icon name="cancel" class="input-cancel"/>
+    </div>
     <button class="btn search-flex-btn" type="button">
+    </button>
+  </div>
+
+  <div class="search search-flex">
+    <div class="search-flex-wrap">
+      <Icon name="find" class="input-search"/>
+      <input type="text" class="input search-flex-input">
+      <Icon name="cancel" class="input-cancel"/>
+    </div>
+    <button class="span-btn search-span-btn" type="button">
+      <span class="default">НАЙТИ</span>
+      <span class="hover">GO</span>
+    </button>
+  </div>
+
+  <div class="search search-grid">
+    <div class="search-flex-wrap">
+      <Icon name="find" class="input-search"/>
+      <input type="text" class="input search-flex-input">
+      <Icon name="cancel" class="input-cancel"/>
+    </div>
+    <button class="span-btn search-span-btn" type="button">
+      <span class="default">НАЙТИ</span>
+      <span class="hover">GO</span>
     </button>
   </div>
 </template>
